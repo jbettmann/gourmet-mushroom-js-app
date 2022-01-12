90185a73afdf0967f863d9bb305d0d89189fb2ef
jbettmann <96999730+jbettmann@users.noreply.github.com>
2022-01-11 22:09:36 -0700

    indented comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,32 @@
 
 
 let mushroomList = [
-  //name of object--------------------------------
+    //name of object--------------------------------
   { name: 'Shiitake',
-  //market value of object per lbs
+    //market value of object per lbs
     value: 11,
-  //flavor notes of ojbect
+    //flavor notes of ojbect
     flavor: ['nutty', 'meaty']
   }
-  //name of object--------------------------------
+    //name of object--------------------------------
   { name: 'Oyster',
-  //market value of object per lbs
+    //market value of object per lbs
     value: 12,
-  //flavor notes of ojbect
+    //flavor notes of ojbect
     flavor: ['earthy', 'peppery', 'sellfish']
   }
-  //name of object--------------------------------
+    //name of object--------------------------------
   { name: 'Lion\'s Mane',
-  //market value of object per lbs
+    //market value of object per lbs
     value: 10,
-  //flavor notes of ojbect
+    //flavor notes of ojbect
     flavor: ['crab', 'lobster']
   }
-  //name of object--------------------------------
+    //name of object--------------------------------
   { name: 'Chanterelle',
-  //market value of object per lbs
+    //market value of object per lbs
     value: 200,
-  //flavor notes of ojbect
+    //flavor notes of ojbect
     flavor: ['fruity', 'nutty']
   }
 ];
