00cb7aecf577247fa5d20dc6c6866185f38326be
jbettmann <96999730+jbettmann@users.noreply.github.com>
2022-01-11 22:06:32 -0700

    added object Oyster w/ name, value and flavor keys

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,10 @@ let mushroomList = [
   //flavor notes of ojbect
     flavor: ['nutty', 'meaty']
   }
+  //name of object--------------------------------
+  { name: 'Oyster',
+  //market value of object per lbs
+    value: 12,
+  //flavor notes of ojbect
+    flavor: ['earthy', 'peppery', 'sellfish']
+  }
