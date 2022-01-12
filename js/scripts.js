c634bdbdc453af462f6a815d8271b4792d7377c7
jbettmann <96999730+jbettmann@users.noreply.github.com>
2022-01-11 22:05:56 -0700

    added object Shiitake w/ name, value and flavor keys

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 
 
 let mushroomList = [
+  //name of object--------------------------------
+  { name: 'Shiitake',
+  //market value of object per lbs
+    value: 11,
+  //flavor notes of ojbect
+    flavor: ['nutty', 'meaty']
+  }
