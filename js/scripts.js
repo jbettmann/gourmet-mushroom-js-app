826b6f8dc51a2c2c5eaa4e671aa9631dc0850fbb
jbettmann <96999730+jbettmann@users.noreply.github.com>
2022-01-11 22:11:13 -0700

    added commas to array list

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,21 @@ let mushroomList = [
     value: 11,
     //flavor notes of ojbect
     flavor: ['nutty', 'meaty']
-  }
+  },
     //name of object--------------------------------
   { name: 'Oyster',
     //market value of object per lbs
     value: 12,
     //flavor notes of ojbect
     flavor: ['earthy', 'peppery', 'sellfish']
-  }
+  },
     //name of object--------------------------------
   { name: 'Lion\'s Mane',
     //market value of object per lbs
     value: 10,
     //flavor notes of ojbect
     flavor: ['crab', 'lobster']
-  }
+  },
     //name of object--------------------------------
   { name: 'Chanterelle',
     //market value of object per lbs
