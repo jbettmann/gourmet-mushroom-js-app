6972360d41d301721d06c8a5627295eca0f1dc3c
jbettmann <96999730+jbettmann@users.noreply.github.com>
2022-01-14 17:33:33 -0700

    Update comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,38 +3,39 @@
 let mushroomList = [
     //name of object--------------------------------
   { name: 'Shiitake',
-    //market value of object per lbs
+    //market value of object $/lbs
     value: 11,
     //flavor notes of ojbect
     flavor: ['nutty', 'meaty']
   },
     //name of object--------------------------------
   { name: 'Oyster',
-    //market value of object per lbs
+    //market value of object $/lbs
     value: 12,
     //flavor notes of ojbect
-    flavor: ['earthy', 'peppery', 'sellfish']
+    flavor: ['earthy', 'peppery', 'shellfish']
   },
     //name of object--------------------------------
   { name: 'Lion\'s Mane',
-    //market value of object per lbs
+    //market value of object $/lbs
     value: 10,
     //flavor notes of ojbect
     flavor: ['crab', 'lobster']
   },
     //name of object--------------------------------
   { name: 'Chanterelle',
-    //market value of object per lbs
+    //market value of object $/lbs
     value: 200,
     //flavor notes of ojbect
     flavor: ['fruity', 'nutty']
   },
     //name of object--------------------------------
   { name: 'White Truffle',
-    //market value of object per lbs
+    //market value of object $/lbs
     value: 4500,
     //flavor notes of ojbect
     flavor: ['garlic', 'shallots']
+  }
 ];
 
 
