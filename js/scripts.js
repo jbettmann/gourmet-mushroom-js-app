262a4cd2d236a0fd9239ead6eaf65ca4f77dd967
jbettmann <96999730+jbettmann@users.noreply.github.com>
2022-01-18 00:04:30 -0700

    created IIFE for the mushroomList

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,48 @@
 
 
-let mushroomList = [
-    //name of object--------------------------------
-  { name: 'Shiitake',
-    //market value of object $/lbs
-    value: 11,
-    //flavor notes of ojbect
-    flavor: ['nutty', 'meaty']
-  },
-    //name of object--------------------------------
-  { name: 'Oyster',
-    //market value of object $/lbs
-    value: 12,
-    //flavor notes of ojbect
-    flavor: ['earthy', 'peppery', 'shellfish']
-  },
-    //name of object--------------------------------
-  { name: 'Lion\'s Mane',
-    //market value of object $/lbs
-    value: 10,
-    //flavor notes of ojbect
-    flavor: ['crab', 'lobster']
-  },
-    //name of object--------------------------------
-  { name: 'Chanterelle',
-    //market value of object $/lbs
-    value: 200,
-    //flavor notes of ojbect
-    flavor: ['fruity', 'nutty']
-  },
-    //name of object--------------------------------
-  { name: 'White Truffle',
-    //market value of object $/lbs
-    value: 4500,
-    //flavor notes of ojbect
-    flavor: ['garlic', 'shallots']
+//  IIFE for mushroomList to avoid accidentally accessing the Global state
+let mushroomRepo = (function () {
+  let mushroomList = [
+      //name of object--------------------------------
+    { name: 'Shiitake',
+      //market value of object $/lbs
+      value: 11,
+      //flavor notes of ojbect
+      flavor: ['nutty', 'meaty']
+    },
+      //name of object--------------------------------
+    { name: 'Oyster',
+      //market value of object $/lbs
+      value: 12,
+      //flavor notes of ojbect
+      flavor: ['earthy', 'peppery', 'shellfish']
+    },
+      //name of object--------------------------------
+    { name: 'Lion\'s Mane',
+      //market value of object $/lbs
+      value: 10,
+      //flavor notes of ojbect
+      flavor: ['crab', 'lobster']
+    },
+      //name of object--------------------------------
+    { name: 'Chanterelle',
+      //market value of object $/lbs
+      value: 200,
+      //flavor notes of ojbect
+      flavor: ['fruity', 'nutty']
+    },
+      //name of object--------------------------------
+    { name: 'White Truffle',
+      //market value of object $/lbs
+      value: 4500,
+      //flavor notes of ojbect
+      flavor: ['garlic', 'shallots']
+    }
+  ];
+
+  // accesses mushroomList array
+  function getAll() {
+    return mushroomList;
   }
 ];
 
